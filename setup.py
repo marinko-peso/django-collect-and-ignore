f4aa6f67b36996bb4aa13be407bbca8a8159c1e0
marinko <marinko.peso@gmail.com>
2018-07-11 13:24:12 +0200

    bump version :goat:

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-collect-and-ignore',
-    version='0.1.0',
+    version='0.1.1',
     description='Custom collectstatic for django with ignoring commonly copied unnecessary files',
     url='https://github.com/marinko-peso/django-collect-and-ignore',
     author='Marinko Peso',
